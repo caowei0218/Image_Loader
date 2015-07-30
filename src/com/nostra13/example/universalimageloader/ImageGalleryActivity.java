4c1eb5f6a5fedebcc3d829c4ce4095f872963e79
曹溦 <caowei0218@qq.com>
2015-07-30 23:05:29 +0800

    图片网络加载框架

---
+++ src/com/nostra13/example/universalimageloader/ImageGalleryActivity.java
@@ -0,0 +1,88 @@
+package com.nostra13.example.universalimageloader;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+
+import com.nostra13.example.universalimageloader.Constants.Extra;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+
+/**
+ * 画廊视图Activity
+ * @author Sergey Tarasevich (nostra13[at]gmail[dot]com)
+ */
+@SuppressWarnings("deprecation")
+public class ImageGalleryActivity extends BaseActivity {
+
+	String[] imageUrls;
+
+	DisplayImageOptions options;
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.ac_image_gallery);
+
+		Bundle bundle = getIntent().getExtras();
+		imageUrls = bundle.getStringArray(Extra.IMAGES);
+
+		options = new DisplayImageOptions.Builder()
+			.showStubImage(R.drawable.ic_stub)
+			.showImageForEmptyUri(R.drawable.ic_empty)
+			.showImageOnFail(R.drawable.ic_error)
+			.cacheInMemory(true)
+			.cacheOnDisc(true)
+			.bitmapConfig(Bitmap.Config.RGB_565)
+			.build();
+
+		// 自API 16之后就被抛弃了
+		Gallery gallery = (Gallery) findViewById(R.id.gallery);
+		gallery.setAdapter(new ImageGalleryAdapter());
+		gallery.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				startImagePagerActivity(position);
+			}
+		});
+	}
+
+	private void startImagePagerActivity(int position) {
+		Intent intent = new Intent(this, ImagePagerActivity.class);
+		intent.putExtra(Extra.IMAGES, imageUrls);
+		intent.putExtra(Extra.IMAGE_POSITION, position);
+		startActivity(intent);
+	}
+
+	private class ImageGalleryAdapter extends BaseAdapter {
+		@Override
+		public int getCount() {
+			return imageUrls.length;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return position;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ImageView imageView = (ImageView) convertView;
+			if (imageView == null) {
+				imageView = (ImageView) getLayoutInflater().inflate(R.layout.item_gallery_image, parent, false);
+			}
+			imageLoader.displayImage(imageUrls[position], imageView, options);
+			return imageView;
+		}
+	}
+}
